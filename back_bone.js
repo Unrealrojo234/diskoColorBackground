c9297db64fee01f7a464750a97b4aa4cd10e221b
Ryan Otieno <otienoryan812@gmail.com>
2024-06-01 05:03:03 -0700

    This is first time commit

---
+++ back_bone.js
@@ -0,0 +1,19 @@
+
+/* document.getElementById("random_button").onclick = function(){
+    random_color();
+} */
+function random_color(){
+    const red = Math.floor(Math.random()*250+1);
+    const green = Math.floor(Math.random()*250+1);
+    const blue = Math.floor(Math.random()*250+1);
+
+    const color = `rgb(${red},${green},${blue})`;
+    
+    document.body.style.backgroundColor = color;
+    
+}
+
+function timer(){
+    setInterval(random_color,500)
+}
+
